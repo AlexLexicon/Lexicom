2298ac5f8102bf48a9f67301ba6e7a4c129746b5
AlexLexicon <Alexrs.business@outlook.com>
2023-04-17 21:22:09 -0700

    added PackagePath ti readme.md includes

---
+++ Lexicom.DependencyInjection.Primitives/DependencyInjectionPrimitivesServiceBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Lexicom.DependencyInjection.Primitives;
+public interface IDependencyInjectionPrimitivesServiceBuilder
+{
+    IServiceCollection Services { get; }
+}
+public class DependencyInjectionPrimitivesServiceBuilder : IDependencyInjectionPrimitivesServiceBuilder
+{
+    /// <exception cref="ArgumentNullException"/>
+    public DependencyInjectionPrimitivesServiceBuilder(IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        Services = services;
+    }
+
+    public IServiceCollection Services { get; }
+}
---
+++ Lexicom.DependencyInjection.Primitives/Extensions/DependencyInjectionPrimitivesServiceBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Lexicom.DependencyInjection.Primitives.Extensions;
+public static class DependencyInjectionPrimitivesServiceBuilderExtensions
+{
+    /// <exception cref="ArgumentNullException"/>
+    public static IDependencyInjectionPrimitivesServiceBuilder AddTimeProvider(this IDependencyInjectionPrimitivesServiceBuilder builder)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+
+        builder.Services.AddSingleton<ITimeProvider>(TimeProvider.System);
+
+        return builder;
+    }
+
+    /// <exception cref="ArgumentNullException"/>
+    public static IDependencyInjectionPrimitivesServiceBuilder AddGuidProvider(this IDependencyInjectionPrimitivesServiceBuilder builder)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+
+        builder.Services.AddSingleton<IGuidProvider, GuidProvider>();
+
+        return builder;
+    }
+}
