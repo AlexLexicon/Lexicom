707259d4d6513b7eff6f227bc973fa9a25543497
AlexLexicon <Alexrs.business@outlook.com>
2023-04-21 09:10:34 -0700

    renamed Aggregate to Slice which is more obvious what it does

--- Lexicom.EntityFramework.Amenities/Slice.cs
+++ Lexicom.EntityFramework.Amenities/Slice.cs
@@ -6,32 +6,32 @@ namespace Lexicom.EntityFramework.Amenities;
 //https://github.com/dotnet/runtime/issues/1808 -> https://github.com/dotnet/runtime/issues/63791
 //instead we can do the following with newtonsoft
 //https://stackoverflow.com/questions/58193757/is-it-possible-to-replicate-the-behaviour-of-jsonobjectattribute
-public class Aggregate<T> : IEnumerable<T>
+public class Slice<T> : IEnumerable<T>
 {
     /*
-     * an Aggregate is used for results from a query operation
+     * a Slice is used for results from a query operation
      * where you want to know how many items could be returned 
      * but only a slice or subset was returned
      * 
      * example: query users that are active but only take '5'
-     * the Aggregate's TotalCount might be '100' 
-     * but the Slice.Count() is only '5' since thats all that was taken
+     * the Slice's 'TotalCount' might be '100' if there are 100 active users 
+     * but the Slice.Count() (The 'Values' enumerable count) is only '5'
      */
 
     /// <exception cref="ArgumentNullException"/>
-    public Aggregate(
+    public Slice(
         int totalCount, 
         IEnumerable<T> slice)
     {
         ArgumentNullException.ThrowIfNull(slice);
 
         TotalCount = totalCount;
-        Slice = slice;
+        Values = slice;
     }
 
     public int TotalCount { get; }
-    public IEnumerable<T> Slice { get; }
+    public IEnumerable<T> Values { get; }
 
-    public IEnumerator<T> GetEnumerator() => Slice.GetEnumerator();
+    public IEnumerator<T> GetEnumerator() => Values.GetEnumerator();
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
