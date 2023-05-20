0312c0c7105c57756268ec1d022dc272664814bb
AlexLexicon <Alexrs.business@outlook.com>
2023-05-20 16:39:21 -0700

    added the RecursiveSizeInBytes to the Lexicom.Extensions.IO package

---
+++ Lexicom.Extensions.IO/DirectoryInfoExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Lexicom.Extensions.IO;
+public static class DirectoryInfoExtensions
+{
+    /// <exception cref="ArgumentNullException"/>
+    public static long RecursiveSizeInBytes(this DirectoryInfo directoryInfo)
+    {
+        ArgumentNullException.ThrowIfNull(directoryInfo);
+
+        long size = 0;
+
+        FileInfo[] files = directoryInfo.GetFiles();
+        foreach (FileInfo file in files)
+        {
+            size += file.Length;
+        }
+
+        DirectoryInfo[] directories = directoryInfo.GetDirectories();
+        foreach (DirectoryInfo directory in directories)
+        {
+            size += RecursiveSizeInBytes(directory);
+        }
+
+        return size;
+    }
+}
