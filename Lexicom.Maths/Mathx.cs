cd717dcda8c7be7fa73c82ca5bb7e6af85b41c87
AlexLexicon <Alexrs.business@outlook.com>
2023-10-30 17:45:01 -0700

    moved the early return from the 'CreateIntegerOfRepeatedDigitsNTimes' method come after the exception checks and added the exception xml docs

--- Lexicom.Maths/Mathx.cs
+++ Lexicom.Maths/Mathx.cs
@@ -1,17 +1,9 @@
 ﻿namespace Lexicom.Maths;
 public static class Mathx
 {
+    /// <exception cref="ArgumentOutOfRangeException"/>
     public static long CreateIntegerOfRepeatedDigitsNTimes(int digit, int n)
     {
-        if (digit is 0)
-        {
-            //the math below will work with zero
-            //but it is a waste since creating a number
-            //with any number of zeros is still zero
-            //so we can make this branch return early
-            return 0;
-        }
-
         if (digit is < 0 or > 9)
         {
             throw new ArgumentOutOfRangeException(nameof(digit), digit, "Must be a single digit integer (0-9).");
@@ -24,6 +16,15 @@ public static class Mathx
             //possible before the double precision fails
 
             throw new ArgumentOutOfRangeException(nameof(n), n, "Must be greater than '0' and less than '15'.");
+        }
+
+        if (digit is 0)
+        {
+            //the math below will work with zero
+            //but it is a waste since creating a number
+            //with any number of zeros is still zero
+            //so we can make this branch return early
+            return 0;
         }
 
         double pow = Math.Pow(10, n);
