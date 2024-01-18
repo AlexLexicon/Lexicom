22b774641f3e6fe1b902d9d57ceef619caabbc43
AlexLexicon <Alexrs.business@outlook.com>
2024-01-18 13:31:39 -0800

    added missing ArgumentNullExceptions

--- Lexicom.Mvvm/Extensions/MvvmServiceBuilderExtensions.cs
+++ Lexicom.Mvvm/Extensions/MvvmServiceBuilderExtensions.cs
@@ -24,6 +24,7 @@ public static class MvvmServiceBuilderExtensions
     public static IMvvmServiceBuilder AddViewModel<TViewModel>(this IMvvmServiceBuilder builder, Action<IViewModelServiceBuilder> configure) where TViewModel : class
     {
         ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(configure);
 
         return AddViewModel<TViewModel, TViewModel>(builder, configure);
     }
@@ -31,6 +32,7 @@ public static class MvvmServiceBuilderExtensions
     public static IMvvmServiceBuilder AddViewModel<TViewModelService, TViewModelImplementation>(this IMvvmServiceBuilder builder, Action<IViewModelServiceBuilder> configure) where TViewModelService : notnull where TViewModelImplementation : class, TViewModelService
     {
         ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(configure);
 
         builder.Services.AddLexicomMvvmViewModel<TViewModelService, TViewModelImplementation>(configure);
 
