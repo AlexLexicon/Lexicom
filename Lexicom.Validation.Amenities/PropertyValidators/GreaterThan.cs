31e40d19e4ad23489b0b1832ac2c3ed31bd8a5a8
AlexLexicon <Alexrs.business@outlook.com>
2024-02-07 15:49:34 -0800

    fixed the new comparison validators

--- Lexicom.Validation.Amenities/PropertyValidators/GreaterThan.cs
+++ Lexicom.Validation.Amenities/PropertyValidators/GreaterThan.cs
@@ -1,29 +1,26 @@
-﻿using FluentValidation;
+﻿using FluentValidation.Validators;
 
 namespace Lexicom.Validation.Amenities.PropertyValidators;
-public class GreaterThan<T> : AbstractPropertyValidator<T, string?>
+#pragma warning disable CS8631 // The type cannot be used as type parameter in the generic type or method. Nullability of type argument doesn't match constraint type.
+public class GreaterThan<T> : AbstractComparisonPropertyValidator<T, string?>
+#pragma warning restore CS8631 // The type cannot be used as type parameter in the generic type or method. Nullability of type argument doesn't match constraint type.
 {
     public const string NAME = nameof(GreaterThan<T>);
     public const string DEFAULT_MESSAGE_TEMPLATE = "'{PropertyName}' must be greater than {ComparisonValue}.";
 
-    private readonly long _maximum;
-
-    public GreaterThan(long maximum)
+    public GreaterThan(long value) : base(value.ToString())
     {
-        _maximum = maximum;
     }
 
     public override string Name { get; } = NAME;
     public override string DefaultMessageTemplate { get; } = DEFAULT_MESSAGE_TEMPLATE;
+    public override Comparison Comparison => Comparison.GreaterThan;
 
-    /// <exception cref="ArgumentNullException"/>
-    public override bool IsValid(ValidationContext<T> context, string? value)
+    public override bool IsValid(string? value, string? valueToCompare)
     {
-        ArgumentNullException.ThrowIfNull(context);
-
-        if (long.TryParse(value, out long longValue))
+        if (long.TryParse(value, out long longValue) && long.TryParse(valueToCompare, out long longValueToCompare))
         {
-            return longValue > _maximum;
+            return longValue > longValueToCompare;
         }
 
         return true;
--- Lexicom.Validation.Amenities/PropertyValidators/LessThan.cs
+++ Lexicom.Validation.Amenities/PropertyValidators/LessThan.cs
@@ -1,29 +1,26 @@
-﻿using FluentValidation;
+﻿using FluentValidation.Validators;
 
 namespace Lexicom.Validation.Amenities.PropertyValidators;
-public class LessThan<T> : AbstractPropertyValidator<T, string?>
+#pragma warning disable CS8631 // The type cannot be used as type parameter in the generic type or method. Nullability of type argument doesn't match constraint type.
+public class LessThan<T> : AbstractComparisonPropertyValidator<T, string?>
+#pragma warning restore CS8631 // The type cannot be used as type parameter in the generic type or method. Nullability of type argument doesn't match constraint type.
 {
     public const string NAME = nameof(LessThan<T>);
     public const string DEFAULT_MESSAGE_TEMPLATE = "'{PropertyName}' must be less than {ComparisonValue}.";
 
-    private readonly long _minimum;
-
-    public LessThan(long minimum)
+    public LessThan(long value) : base(value.ToString())
     {
-        _minimum = minimum;
     }
 
     public override string Name { get; } = NAME;
     public override string DefaultMessageTemplate { get; } = DEFAULT_MESSAGE_TEMPLATE;
+    public override Comparison Comparison => Comparison.LessThan;
 
-    /// <exception cref="ArgumentNullException"/>
-    public override bool IsValid(ValidationContext<T> context, string? value)
+    public override bool IsValid(string? value, string? valueToCompare)
     {
-        ArgumentNullException.ThrowIfNull(context);
-
-        if (long.TryParse(value, out long longValue))
+        if (long.TryParse(value, out long longValue) && long.TryParse(valueToCompare, out long longValueToCompare))
         {
-            return longValue < _minimum;
+            return longValue < longValueToCompare;
         }
 
         return true;
