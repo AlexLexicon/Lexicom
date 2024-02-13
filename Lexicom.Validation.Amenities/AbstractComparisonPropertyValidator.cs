4b57a9ff2bfcebed43fdb5788d6c6cc8431fc354
AlexLexicon <Alexrs.business@outlook.com>
2024-02-13 11:36:31 -0800

    improved comparision property validators

---
+++ Lexicom.Validation.Amenities/AbstractComparisonPropertyValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation.Validators;
+using System.Reflection;
+
+namespace Lexicom.Validation.Amenities;
+public abstract class AbstractComparisonPropertyValidator<T, TProperty> : AbstractComparisonValidator<T, TProperty> where TProperty : IComparable<TProperty>, IComparable
+{
+    public AbstractComparisonPropertyValidator(TProperty value) : base(value)
+    {
+    }
+    public AbstractComparisonPropertyValidator(Func<T, (bool HasValue, TProperty Value)> valueToCompareFunc, MemberInfo member, string memberDisplayName) : base(valueToCompareFunc, member, memberDisplayName)
+    {
+    }
+
+    public abstract string DefaultMessageTemplate { get; }
+
+    protected override string GetDefaultMessageTemplate(string? errorCode)
+    {
+        string? localizedMessageTemplate = Localized(errorCode, Name);
+
+        if (!string.IsNullOrWhiteSpace(localizedMessageTemplate))
+        {
+            return localizedMessageTemplate;
+        }
+
+        return DefaultMessageTemplate ?? throw new NullReferenceException($"{nameof(DefaultMessageTemplate)} was null");
+    }
+}
--- Lexicom.Validation.Amenities/PropertyValidators/GreaterThanOrEqualTo.cs
+++ Lexicom.Validation.Amenities/PropertyValidators/GreaterThanOrEqualTo.cs
@@ -1,29 +1,26 @@
-﻿using FluentValidation;
+﻿using FluentValidation.Validators;
 
 namespace Lexicom.Validation.Amenities.PropertyValidators;
-public class GreaterThanOrEqualTo<T> : AbstractPropertyValidator<T, string?>
+#pragma warning disable CS8631 // The type cannot be used as type parameter in the generic type or method. Nullability of type argument doesn't match constraint type.
+public class GreaterThanOrEqualTo<T> : AbstractComparisonPropertyValidator<T, string?>
+#pragma warning restore CS8631 // The type cannot be used as type parameter in the generic type or method. Nullability of type argument doesn't match constraint type.
 {
     public const string NAME = nameof(GreaterThanOrEqualTo<T>);
     public const string DEFAULT_MESSAGE_TEMPLATE = "'{PropertyName}' must be greater than or equal to {ComparisonValue}.";
 
-    private readonly long _maximum;
-
-    public GreaterThanOrEqualTo(long maximum)
+    public GreaterThanOrEqualTo(long value) : base(value.ToString())
     {
-        _maximum = maximum;
     }
 
     public override string Name { get; } = NAME;
     public override string DefaultMessageTemplate { get; } = DEFAULT_MESSAGE_TEMPLATE;
+    public override Comparison Comparison => Comparison.GreaterThanOrEqual;
 
-    /// <exception cref="ArgumentNullException"/>
-    public override bool IsValid(ValidationContext<T> context, string? value)
+    public override bool IsValid(string? value, string? valueToCompare)
     {
-        ArgumentNullException.ThrowIfNull(context);
-
-        if (long.TryParse(value, out long longValue))
+        if (value is not null && long.TryParse(valueToCompare, out long longValueToCompare))
         {
-            return longValue >= _maximum;
+            return value.Length >= longValueToCompare;
         }
 
         return true;
