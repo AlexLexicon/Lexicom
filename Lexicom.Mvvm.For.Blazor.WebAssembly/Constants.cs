3466748625ed7181327c11b7b0378505f39fd6e4
AlexLexicon <Alexrs.business@outlook.com>
2024-02-23 13:36:50 -0800

    made it so blazor view now detects changes to observable collections and will auto invoke loaded commands on page load

---
+++ Lexicom.Mvvm.For.Blazor.WebAssembly/Constants.cs
@@ -0,0 +1,5 @@
+﻿namespace Lexicom.Mvvm.For.Blazor.WebAssembly;
+public static class Constants
+{
+    public const string COMMAND_LOADED = "LoadedCommand";
+}
---
+++ Lexicom.Mvvm.For.Blazor.WebAssembly/Exceptions/LoadedCommandNotValidException.cs
@@ -0,0 +1,6 @@
+﻿using System.Windows.Input;
+
+namespace Lexicom.Mvvm.For.Blazor.WebAssembly.Exceptions;
+public class LoadedCommandNotValidException(object? viewModel) : Exception($"The view model '{viewModel?.GetType().FullName ?? "null"}' had a property called '{Constants.COMMAND_LOADED}' which was automatically bound to the 'OnInitializedAsync' of the view but the command was not of the type '{typeof(ICommand).FullName}'")
+{
+}
