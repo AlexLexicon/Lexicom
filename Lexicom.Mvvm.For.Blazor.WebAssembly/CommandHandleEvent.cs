3f748f454a7a2337a6d2073a027c1c15c252d1e4
AlexLexicon <Alexrs.business@outlook.com>
2024-02-25 02:39:34 -0800

    updated the bind extensions for commands to support the bind:after attribute and accept parameters correctly utlizing the IHandleEvent interface

---
+++ Lexicom.Mvvm.For.Blazor.WebAssembly/CommandHandleEvent.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Microsoft.AspNetCore.Components;
+using System.Windows.Input;
+
+namespace Lexicom.Mvvm.For.Blazor.WebAssembly;
+internal class CommandHandleEvent : IHandleEvent
+{
+    private readonly ICommand _command;
+
+    /// <exception cref="ArgumentNullException"/>
+    public CommandHandleEvent(ICommand command)
+    {
+        ArgumentNullException.ThrowIfNull(command);
+
+        _command = command;
+    }
+
+    public async Task HandleEventAsync(EventCallbackWorkItem item, object? arg)
+    {
+        if (_command is IAsyncRelayCommand asyncRelayCommand)
+        {
+            await asyncRelayCommand.ExecuteAsync(arg);
+        }
+        else
+        {
+            _command.Execute(arg);
+        }
+    }
+}
--- Lexicom.Mvvm.For.Blazor.WebAssembly/Extensions/CommandExtensions.cs
+++ Lexicom.Mvvm.For.Blazor.WebAssembly/Extensions/CommandExtensions.cs
@@ -4,11 +4,24 @@ using System.Windows.Input;
 namespace Lexicom.Mvvm.For.Blazor.WebAssembly.Extensions;
 public static class CommandExtensions
 {
-    public static EventCallback Bind(this ICommand command, object? parameter = null)
+    /// <exception cref="ArgumentNullException"/>
+    public static Action BindAfter(this ICommand command, object? parameter = null)
     {
-        return new EventCallback(null, () =>
+        ArgumentNullException.ThrowIfNull(command);
+
+        return () =>
         {
             command.Execute(parameter);
-        });
+        };
+    }
+
+    /// <exception cref="ArgumentNullException"/>
+    public static EventCallback BindEvent(this ICommand command)
+    {
+        ArgumentNullException.ThrowIfNull(command);
+
+        var commandHandleEvent = new CommandHandleEvent(command);
+
+        return new EventCallback(commandHandleEvent, @delegate: null);
     }
 }
