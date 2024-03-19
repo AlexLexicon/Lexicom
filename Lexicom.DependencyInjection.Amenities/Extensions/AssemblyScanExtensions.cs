e4d7b14ce7ad5a58b96c31f03e424457138c84b0
AlexLexicon <Alexrs.business@outlook.com>
2024-03-19 15:39:22 -0700

    removed blank lines

--- Lexicom.DependencyInjection.Amenities/Extensions/AssemblyScanExtensions.cs
+++ Lexicom.DependencyInjection.Amenities/Extensions/AssemblyScanExtensions.cs
@@ -1,8 +1,6 @@
 ﻿namespace Lexicom.DependencyInjection.Amenities.Extensions;
 public static class AssemblyScanExtensions
 {
-
-
     /// <exception cref="ArgumentNullException"/>
     public static IAssemblyScan Register(this IAssemblyScan assemblyScan, Action<Type> registerDelegate)
     {
