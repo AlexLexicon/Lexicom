3a501302be57b58e19ccd27ff503b7e972c63a40
AlexLexicon <Alexrs.business@outlook.com>
2024-08-16 12:58:20 -0700

    added the IntegerExtensions.ToDayOfMonthSuffix extension method

---
+++ Lexicom.Extensions.DateTimes/IntegerExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Lexicom.Extensions.DateTimes;
+public static class IntegerExtensions
+{
+    public static string ToDayOfMonthSuffix(this int dayOfMonth)
+    {
+        double below100 = dayOfMonth % 10;
+        double above100 = dayOfMonth % 100;
+
+        if (below100 is 1 && above100 is not 11)
+        {
+            return "st";
+        }
+
+        if (below100 is 2 && above100 is not 12)
+        {
+            return "nd";
+        }
+
+        if (below100 is 3 && above100 is not 13)
+        {
+            return "rd";
+        }
+
+        return "th";
+    }
+}
