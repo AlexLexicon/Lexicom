0f3e5def1ee3fc48c44bac9ac0dc91de17d9141a
AlexLexicon <Alexrs.business@outlook.com>
2024-08-18 20:19:12 -0700

    added the cryptography.for.blazor.webassembly project

---
+++ Lexicom.Cryptography.For.Blazor.WebAssembly/Extensions/BlazorWebAssemblyServiceBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Lexicom.Cryptography.Extensions;
+using Lexicom.Supports.Blazor.WebAssembly;
+
+namespace Lexicom.Cryptography.For.Blazor.WebAssembly.Extensions;
+public static class BlazorWebAssemblyServiceBuilderExtensions
+{
+    /// <exception cref="ArgumentNullException"/>
+    public static IBlazorWebAssemblyServiceBuilder AddMvvm(this IBlazorWebAssemblyServiceBuilder builder, Action<ICryptographyServiceBuilder>? configure = null)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+
+        builder.WebAssemblyHostBuilder.Services.AddLexicomCryptography(configure);
+
+        return builder;
+    }
+}
