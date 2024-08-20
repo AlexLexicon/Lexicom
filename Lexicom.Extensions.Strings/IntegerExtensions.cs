dc0ad3f540b4827f01710fb9a672679b48942d85
AlexLexicon <Alexrs.business@outlook.com>
2024-08-20 05:56:49 -0700

    added a GetCountWord method to the IntegerExtensions

--- Lexicom.Extensions.Strings/IntegerExtensions.cs
+++ Lexicom.Extensions.Strings/IntegerExtensions.cs
@@ -23,4 +23,22 @@ public static class IntegerExtensions
 
         return "th";
     }
+
+    public static string GetCountWord(this int number, string def = "many")
+    {
+        return number switch
+        {
+            1 => "one",
+            2 => "two",
+            3 => "three",
+            4 => "four",
+            5 => "five",
+            6 => "six",
+            7 => "seven",
+            8 => "eight",
+            9 => "nine",
+            10 => "ten",
+            _ => def,
+        };
+    }
 }
