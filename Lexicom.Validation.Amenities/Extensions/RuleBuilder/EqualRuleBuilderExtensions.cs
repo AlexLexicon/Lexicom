5060e37c3db605c86099c3e0e872633e4bd0221e
AlexLexicon <Alexrs.business@outlook.com>
2024-08-20 23:44:11 -0700

    added overloads for the equal property validator from fluent validations

---
+++ Lexicom.Validation.Amenities/Extensions/RuleBuilder/EqualRuleBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Lexicom.Validation.Amenities.Extensions;
+public static class EqualRuleBuilderExtensions
+{
+    /// <exception cref="ArgumentNullException"/>
+    public static IRuleBuilderOptions<T, TProperty> Equal<T, TProperty>(this IRuleBuilder<T, TProperty> ruleBuilder, Func<TProperty> toCompareFunc, IEqualityComparer<TProperty>? comparer = null)
+    {
+        ArgumentNullException.ThrowIfNull(ruleBuilder);
+        ArgumentNullException.ThrowIfNull(toCompareFunc);
+
+        return ruleBuilder.SetValidator(new FluentValidation.Validators.EqualValidator<T, TProperty>(_ => toCompareFunc.Invoke(), member: null, memberDisplayName: null, comparer));
+    }
+    /// <exception cref="ArgumentNullException"/>
+    public static IRuleBuilderOptions<T, TProperty> Equal<T, TProperty>(this IRuleBuilder<T, TProperty> ruleBuilder, Func<T, TProperty> toCompareFunc, IEqualityComparer<TProperty>? comparer = null)
+    {
+        ArgumentNullException.ThrowIfNull(ruleBuilder);
+        ArgumentNullException.ThrowIfNull(toCompareFunc);
+
+        return ruleBuilder.SetValidator(new FluentValidation.Validators.EqualValidator<T, TProperty>(toCompareFunc, member: null, memberDisplayName: null, comparer));
+    }
+}
