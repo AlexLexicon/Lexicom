84478fadbbc2f30b3a80325890e4caf297262a52
AlexLexicon <Alexrs.business@outlook.com>
2024-12-05 08:20:37 -0800

    added support for wpf for logging

---
+++ Lexicom.Logging.For.Wpf/Extensions/WpfServiceBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Lexicom.Logging.Extensions;
+using Lexicom.Supports.Wpf;
+using Microsoft.Extensions.Configuration;
+
+namespace Lexicom.Logging.For.Wpf.Extensions;
+public static class WpfServiceBuilderExtensions
+{
+    /// <exception cref="ArgumentNullException"/>
+    public static IWpfServiceBuilder AddSmtp(this IWpfServiceBuilder builder, IConfiguration configuration)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+
+        builder.Services.AddLexicomLogging(configuration);
+
+        return builder;
+    }
+}
