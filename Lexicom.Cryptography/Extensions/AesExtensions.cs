29b508a38f8d024ba9b2dcf042e0de75aae21541
AlexLexicon <Alexrs.business@outlook.com>
2026-06-13 00:15:53 -0700

    better Aes CalculateSecretSize

--- Lexicom.Cryptography/Extensions/AesExtensions.cs
+++ Lexicom.Cryptography/Extensions/AesExtensions.cs
@@ -8,14 +8,26 @@ public static class AesExtensions
     public static SecretBitSize CalculateSecretSize(this Aes aes, byte[] secretKey)
     {
         ArgumentNullException.ThrowIfNull(aes);
+        ArgumentNullException.ThrowIfNull(secretKey);
 
         var allowedSizes = new HashSet<int>();
 
         KeySizes[] ks = aes.LegalKeySizes;
         foreach (KeySizes item in ks)
         {
-            allowedSizes.Add(item.MinSize);
-            allowedSizes.Add(item.MaxSize);
+            //a KeySizes describes a range of sizes from MinSize to MaxSize in steps of SkipSize
+            //for example AES reports min 128 max 256 skip 64 which includes the 192 bit size
+            if (item.SkipSize is 0)
+            {
+                allowedSizes.Add(item.MinSize);
+            }
+            else
+            {
+                for (int size = item.MinSize; size <= item.MaxSize; size += item.SkipSize)
+                {
+                    allowedSizes.Add(size);
+                }
+            }
         }
 
         int bitsSize = secretKey.Length * 8;
